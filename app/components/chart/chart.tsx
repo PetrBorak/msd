c23fff076eff4aaac7328125d2e3c3818b0869df
Petr Borak <borak.petr@gmail.com>
2024-02-11 20:29:32 +0100

    rename import Chart

--- app/components/chart/chart.tsx
+++ app/components/chart/chart.tsx
@@ -1,9 +1,9 @@
 'use client'
-import { Chart } from '@antv/g2';
+import { Chart as ChartAnt } from '@antv/g2';
 import { useEffect, useRef } from 'react';
 import dynamic from 'next/dynamic'
 
-export default function Chart<data>({data, renderer}: { data: data, renderer: (container: HTMLElement, data: data) => Chart}) {
+export default function Chart<data>({data, renderer}: { data: data, renderer: (container: HTMLElement, data: data) => ChartAnt}) {
     const container = useRef(null);
     const chart: any = useRef(null);
 
